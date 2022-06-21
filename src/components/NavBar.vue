ae1cef7c341a3f2b60d6a350e87c80344424fcf4
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-06-21 10:36:55 +0200

    Added a new Logo and a favicon to the project

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-
+    <nav class="navbar">
+        <img :src="require('../assets/Logo.png')" alt="✔" class="logo">
+        <h2 class="logo-caption">Dawid Barański</h2>
+    </nav>
 </template>
 
 <style lang="scss">
