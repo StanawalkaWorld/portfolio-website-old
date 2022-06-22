3852f2b1fcb790c44e465e5208a941266feaf498
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-06-22 12:31:48 +0200

    Added new components to use in UI

---
+++ src/components/TwoColumnContent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="two-column-container">
+        <div class="two-column">
+            <slot name="first"></slot>
+        </div>
+        <div class="two-column">
+            <slot name="second"></slot>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.two-column-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+
+    .two-column {
+        width: 50%;
+    }
+
+    @media screen and (max-width: 768) {
+        flex-direction: column;
+
+        .two-column {
+            width: 100%;
+        }
+    }
+}
+</style>
---
+++ src/components/UIElements/TitledContent.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+type alignement = "left" | "center" | "right" | "justified";
+
+const props = defineProps<{
+    title: string
+    headerAlign: alignement
+    paragraphAlign: alignement
+}>();
+
+const headerStyle = {
+    textAlign: props.headerAlign
+};
+
+const paragraphStyle = {
+    textAlign: props.paragraphAlign
+}
+
+</script>
+
+<template>
+    <h2 class="content-header" :style="headerStyle">
+        {{ title }}
+    </h2>
+    <p class="content-paragraph">
+        <slot></slot>
+    </p>
+</template>
+
+<style lang="scss" scoped>
+.content-header {
+
+}
+</style>
