dd82e80cf5585c9601b33ae0a34862178a34ad8a
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-06-22 14:57:36 +0200

    Removed unnecessary button and translation.

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -8,7 +8,6 @@ const lang = useLanguageStore();
     <!-- TODO: Introduction to who I am and stuff -->
     <h1>{{ lang.translationFor("content", "testing.header") }}</h1>
     <p>{{ lang.translationFor("content", "testing.paragraph") }} <a class="mail" href="mailto:dawid.baranski.ti@gmail.com">dawid.baranski.ti@gmail.com</a></p>
-    <button @click="lang.switchLanguage">{{ lang.translationFor("content", "testing.langbutton") }}</button>
 </template>
 
 <style lang="scss" scoped>
