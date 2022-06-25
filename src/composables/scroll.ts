30549d253cf06c8c60b7b49e6f1d2d55a583355f
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-06-25 10:55:18 +0200

    Changed the behaviour of the smooth scroll

--- src/composables/scroll.ts
+++ src/composables/scroll.ts
@@ -1,6 +1,6 @@
 export const useSmoothScroll = () => {
     function scrollTo(elem: string): void {
-        document.querySelector(elem)?.scrollIntoView({ behavior: "smooth" });
+        document.querySelector(elem)?.scrollIntoView({ behavior: "smooth", block: "center" });
     }
 
     return {
