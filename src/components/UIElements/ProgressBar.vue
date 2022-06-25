a9cb01b2ef87348d49ee35e0ffb6b2f9dfa06065
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-06-25 11:11:59 +0200

    Created a progress bar component.

---
+++ src/components/UIElements/ProgressBar.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+
+
+const props = defineProps({
+    value: {
+        required: true,
+        type: Number,
+
+        validator(value) {
+            return value > 0 && value <= 100;
+        }
+    },
+    progessColor: {
+        required: false,
+        default: "#C00",
+        type: String
+    },
+
+    prominent: {
+        required: false,
+        default: false
+    }
+});
+
+const propHeight = computed(() => {
+    return ((prominent) ? "20px" : "10px");
+});
+
+</script>
+
+<template>
+    <div class="bar">
+        <div 
+            class="bar-progress" 
+            :style="{ width: value, backgroundColor: progessColor, height: propHeight }">
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.bar {
+    border-radius: 10px;
+    background: $cm-light-accent;
+
+    width: 100%;
+}
+.bar-progress {
+    border-radius: 10px;
+}
+
+</style>
